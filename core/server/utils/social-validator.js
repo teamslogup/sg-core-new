b5fd882c86c55b188055b2e5413f76cf22044b8d
leehwarang <ceo@slogup.com>
2016-07-31 04:34:15 +0900

    kakao account validation.

--- core/server/utils/social-validator.js
+++ core/server/utils/social-validator.js
@@ -1,9 +1,9 @@
+var request = require('request');
 module.exports = {
     facebookValidadtor: function (uid, secret, callback) {
         if (process.env.NODE_ENV == "test") {
             return callback(200);
         }
-        var request = require('request');
         var rootUri = 'https://graph.facebook.com/me?access_token=';
         var option = {
             method: 'GET',
@@ -26,7 +26,26 @@ module.exports = {
         if (process.env.NODE_ENV == "test") {
             return callback(200);
         }
-        callback(200);
+        var rootUri = 'http://kapi.kakao.com/v1/user/access_token_info';
+        var option = {
+            method: 'GET',
+            uri: rootUri + secret,
+            headers: {
+                'Authorization': "Bearer " + secret
+            }
+        };
+        request(option, function (error, response, body) {
+            if (response.statusCode == 200) {
+                body = JSON.parse(body);
+                if (body.id == uid) {
+                    callback(200);
+                } else {
+                    callback(403);
+                }
+            } else {
+                callback(403);
+            }
+        });
     },
     googleValidadtor: function (uid, secret, callback) {
         if (process.env.NODE_ENV == "test") {
