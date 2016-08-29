7bb75a2bb9b70fbcb6d822d87a6135111e7c02fe
KJY <dusrn@slogup.com>
2016-08-29 21:15:48 +0900

    session module view, controller 추가

--- core/client/modules/users/core.users.module.js
+++ core/client/modules/users/core.users.module.js
@@ -1,9 +1,9 @@
-import usersResources from './services/core.users.constants';
-import User from './services/core.users.model';
-import usersManager from './services/core.users.manager';
+// import usersResources from './services/core.users.constants';
+// import User from './services/core.users.model';
+// import usersManager from './services/core.users.manager';
 
 export default angular.module("core.users", [])
-    .constant("usersResources", usersResources)
-    .factory("User", User)
-    .service("usersManager", usersManager)
+    // .constant("usersResources", usersResources)
+    // .factory("User", User)
+    // .service("usersManager", usersManager)
     .name;
