82f5aa663f23f26f2d6ab54cd3645fd5650d7214
ac12bd <ac12bd@slogup.com>
2016-09-16 03:32:52 +0900

    api-etc-reports: 마이크로타임으로 변경

--- core/server/models/sequelize/report.js
+++ core/server/models/sequelize/report.js
@@ -39,42 +39,74 @@ module.exports = {
             'type': Sequelize.BOOLEAN,
             'allowNull': false,
             'defaultValue': false
-        }
+        },
+        'createdAt': {
+            'type': Sequelize.BIGINT,
+            'allowNull': true
+        },
+        'updatedAt': {
+            'type': Sequelize.BIGINT,
+            'allowNull': true
+        },
+        'deletedAt': {
+            'type': Sequelize.DATE,
+            'allowNull': true
+        },
     },
     options: {
+        'timestamps': true,
         'charset': 'utf8',
-        'paranoid': true,
-        'hooks': {},
+        'createdAt': false,
+        'updatedAt': false,
+        'paranoid': true, // deletedAt 추가. delete안함.
+        'hooks': {
+            'beforeCreate': mixin.options.hooks.microCreatedAt,
+            'beforeBulkUpdate': mixin.options.hooks.useIndividualHooks,
+            'beforeUpdate': mixin.options.hooks.microUpdatedAt
+        },
         'instanceMethods': Sequelize.Utils._.extend(mixin.options.instanceMethods, {}),
         'classMethods': Sequelize.Utils._.extend(mixin.options.classMethods, {
+            'getReportInclude': function () {
+                return [{
+                    'model': sequelize.models.User,
+                    'as': 'author',
+                    'attributes': sequelize.models.User.getUserFields()
+                }];
+            },
             'findReportsByOptions': function (options, callback) {
                 var where = {};
 
                 if (options.authorId !== undefined) where.authorId = options.authorId;
                 if (options.isSolved !== undefined) where.isSolved = options.isSolved;
-                
-                var query = {
-                    'limit': parseInt(options.size),
-                    'where': where,
-                    'include': {
-                        'model': sequelize.models.User,
-                        'as': 'author'
-                    }
-                };
 
                 if (options.searchField) {
-                    query.where[options.searchField] = {
+                    where[options.searchField] = {
                         '$like': "%" + options.searchItem + "%"
                     };
                 }
 
-                query.where.createdAt = {
+                where.createdAt = {
                     '$lt': options.last
                 };
-                
-                if (options.sort !== undefined) query.order = [['createdAt', options.sort]];
 
-                sequelize.models.Report.findAllDataForQuery(query, callback);
+                var query = {
+                    'limit': parseInt(options.size),
+                    'where': where,
+                    'order':[['createdAt', options.sort]],
+                    'include': sequelize.models.Report.getReportInclude()
+                };
+
+                sequelize.models.Report.findAndCountAll(query).then(function (data) {
+                    if (data.rows.length > 0) {
+                        return data;
+                    } else {
+                        throw new errorHandler.CustomSequelizeError(404);
+                    }
+                }).catch(errorHandler.catchCallback(callback)).done(function (data) {
+                    if (data) {
+                        callback(200, data);
+                    }
+                });
             },
             'findReportById': function (id, callback) {
                 sequelize.models.Report.findDataById(id, callback);
