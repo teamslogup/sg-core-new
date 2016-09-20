209970cd0e2cfca776a7886224c232689f88b5f9
KJY <KJY@KJYui-MacBook-Pro.local>
2016-09-20 15:39:40 +0900

    files[k] bug fix

--- core/client/components/file-uploader/core.common.file-uploader.js
+++ core/client/components/file-uploader/core.common.file-uploader.js
@@ -1,8 +1,9 @@
 export default function fileUploader ($http) {
     this.upload = function (prefix, data, files, uploadUrl) {
-        var i = 0;
         var fd = new FormData();
-        for (var k in files) fd.append(prefix + i++, files[k]);
+        for (var i=0; i<files.length; i++) {
+            fd.append(prefix + i, files[i]);
+        }
         for (var k in data) fd.append(k, data[k]);
         return $http.post(uploadUrl, fd, {
             transformRequest: angular.identity,
