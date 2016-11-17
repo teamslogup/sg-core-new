7d344f40a2280ca1ce6032998785b1c437a61e2a
KJY <KJY@KJYui-MacBook-Pro.local>
2016-11-17 17:33:43 +0900

    HTTPS 파일 원복

--- core/server/config/https.js
+++ core/server/config/https.js
@@ -11,12 +11,12 @@ module.exports = function (app) {
     server.http = http.createServer(app);
     if (STD.flag.isUseHttps) {
         var keyPath = __dirname + "/../../../app/server/config/ssl/key.pem",
-            csrPath = __dirname + "/../../../app/server/config/ssl/csr.pem";
-        if (fs.existsSync(keyPath) && fs.existsSync(csrPath)) {
+            certPath = __dirname + "/../../../app/server/config/ssl/cert.pem";
+        if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
             server.isUseHttps = true;
             server.https = https.createServer({
                 key: fs.readFileSync(keyPath, 'utf8'),
-                csr: fs.readFileSync(csrPath, 'utf8')
+                cert: fs.readFileSync(certPath)
             }, app);
         }
     }
