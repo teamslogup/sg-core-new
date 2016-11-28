110e91c3d245f1e78d5ec77fbd2c096fe6f7ff3f
leehwarang <ceo@slogup.com>
2016-11-28 22:02:04 +0900

    remote-session delete 수정.

--- core/server/apis/accounts/session-remote/session-remote.del.js
+++ core/server/apis/accounts/session-remote/session-remote.del.js
@@ -14,16 +14,23 @@ del.logout = function () {
     return function (req, res, next) {
         req.models.LoginHistory.findDataById(req.body.id, function (status, data) {
             if (status == 200) {
-                if (req.user.id != data.userId) {
-                    return res.hjson(req, next, 403);
-                }
-                var sessionId = data.session;
-                req.sessionStore.destroy(sessionId, function(err) {
+                var user = data;
+                req.models.LoginHistory.destroyDataById(req.body.id, null, function(status, data) {
+                    if (status == 204) {
+                        if (req.user.id != user.userId) {
+                            return res.hjson(req, next, 403);
+                        }
+                        var sessionId = user.session;
+                        req.sessionStore.destroy(sessionId, function(err) {
 
-                    if (err) {
-                        res.hjson(req, next, 500);
+                            if (err) {
+                                res.hjson(req, next, 500);
+                            } else {
+                                res.hjson(req, next, 204);
+                            }
+                        });
                     } else {
-                        res.hjson(req, next, 204);
+                        res.hjson(req, next, status, data);
                     }
                 });
             } else {
