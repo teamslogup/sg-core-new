192b6abe6441ef973acf02f86fc9d12a5098c0ad
leehwarang <ceo@slogup.com>
2016-12-05 13:48:35 +0900

    다우페이 코어 연동

--- core/server/config/express.js
+++ core/server/config/express.js
@@ -95,7 +95,7 @@ module.exports = function (sequelize) {
 
     app.use(languageParser(META.local));
     app.use(function(req, res, next) {
-        console.log("thirdParty", util.inspect(req, false, null));
+        // console.log("thirdParty", util.inspect(req, false, null));
         var contentType = (req.headers['Content-type'] || req.headers['Content-Type'] || req.headers['content-Type']  || req.headers['content-type']);
         if (!contentType || contentType.indexOf("xml") == -1) {
             bodyParser.json({limit:CONFIG.app.maxUploadFileSizeMBVersion})(req, res, function() {
