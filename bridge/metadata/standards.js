5319c0c51ca9540daf61f38a0dddef14eeabdd32
KJY <KJY@KJYui-MacBook-Pro.local>
2016-12-13 07:03:50 +0900

    standard NODE_ENV.js 로 분리 완료. app단도 똑같이 구성해야함.

--- bridge/metadata/standards.js
+++ bridge/metadata/standards.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
+var path = require('path');
+
 var utils = require('../utils');
-module.exports = utils.mixFromPath(
-    __dirname,
-    '../../core/server/metadata/standards.js',
-    '../../app/server/metadata/standards.js'
-);
+var coreStd = require('../../core/server/metadata/standards');
+
+if (fs.existsSync(path.resolve(__dirname, '../../app/server/metadata/standards'))) {
+    var appStd = require('../../app/server/metadata/standards');
+    module.exports = utils.mix(coreStd, appStd);
+} else {
+    module.exports = coreStd;
+}
