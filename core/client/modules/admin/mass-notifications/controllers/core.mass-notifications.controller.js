47c52c03b6d963c9ffe7ae1c36f6b3b4041c93b9
KJY <dusrn@slogup.com>
2017-01-03 18:53:45 +0900

    전송관리 페이지 구성 및 디비 변경

--- core/client/modules/admin/mass-notifications/controllers/core.mass-notifications.controller.js
+++ core/client/modules/admin/mass-notifications/controllers/core.mass-notifications.controller.js
@@ -1,4 +1,4 @@
-export default function MassNotificationCtrl ($scope, $rootScope, dialogHandler, loadingHandler, metaManager) {
+export default function MassNotificationCtrl ($scope, $rootScope, dialogHandler, loadingHandler, metaManager, massNotificationsManager) {
     var vm = {};
     if ($scope.vm) {
         vm = $scope.vm;
@@ -6,13 +6,43 @@ export default function MassNotificationCtrl ($scope, $rootScope, dialogHandler,
     var ADMIN = metaManager.std.admin;
 
     $scope.findMassNotifications = findMassNotifications;
+    $scope.findMoreMassNotifications = findMoreMassNotifications;
 
-    $scope.FLAG = metaManager.flag;
-    $scope.massNotifications = {
-        rows: []
-    };
+    $scope.COMMON = metaManager.std.common;
+    $scope.FLAG = metaManager.std.flag;
+    $scope.form = {};
+    $scope.enumKeys = [];
 
     $rootScope.$broadcast(ADMIN.kNavigation, {
         activeNav: ADMIN.moduleMassNotifications
     });
+
+    initForm();
+    findMassNotifications();
+
+    function findMassNotifications () {
+        $scope.massNotifications = {
+            rows: []
+        };
+        var query = {};
+        if ($scope.form.key) query.key = angular.copy($scope.form.key);
+        if ($scope.form.sendType) query.sendType = angular.copy($scope.form.sendType);
+        query.size = $scope.COMMON.defaultLoadingLength;
+        massNotificationsManager.findMassNotifications(query, function (status, data) {
+            if (status == 200) {
+
+            } else {
+
+            }
+        });
+    }
+
+    function findMoreMassNotifications () {
+
+    }
+
+    function initForm () {
+        $scope.form.key = '';
+        $scope.form.sendType = '';
+    }
 }
