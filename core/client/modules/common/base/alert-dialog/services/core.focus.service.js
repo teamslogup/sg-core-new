67d8a0b50ac6fe93853935ba2475a2fd153725d6
Ryu YoungJin <ac12bd@gmail.com>
2017-01-20 15:32:05 +0900

    alert-dialog: 확인 버튼 포커싱

---
+++ core/client/modules/common/base/alert-dialog/services/core.focus.service.js
@@ -0,0 +1,11 @@
+focus.$inject = ['$timeout', '$window'];
+
+export default function focus($timeout, $window) {
+    return function(id) {
+        $timeout(function() {
+            var element = $window.document.getElementById(id);
+            if(element)
+                element.focus();
+        });
+    };
+}
