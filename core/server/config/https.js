021e63041222bc2936abe373cdbc64768ceb63ae
KJY <dusrn@slogup.com>
2017-01-26 16:34:26 +0900

    https 설정 변경

--- core/server/config/https.js
+++ core/server/config/https.js
@@ -12,13 +12,30 @@ module.exports = function (app) {
     };
     server.http = http.createServer(app);
     if (STD.flag.isUseHttps) {
+
         var keyPath = __dirname + "/../../../app/server/config/ssl/" + config.app.keyFile,
             certPath = __dirname + "/../../../app/server/config/ssl/" + config.app.crtFile;
+
+        var options = {
+            key: fs.readFileSync(keyPath, 'utf8'),
+            cert: fs.readFileSync(certPath, 'utf8')
+        };
+
+        var cr = [];
+
+        if (config.app.cr && config.app.cr.length) {
+            for (var i=0; i<config.app.cr.length; i++) {
+                cr.push(fs.readFileSync(__dirname + "/../../../app/server/config/ssl/" + config.app.cr[i]));
+            }
+            options.cr = cr;
+        }
+
+        if (config.app.passphrase) {
+            options.passphrase = config.app.passphrase;
+        }
+
         if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
-            server.https = https.createServer({
-                key: fs.readFileSync(keyPath, 'utf8'),
-                cert: fs.readFileSync(certPath, 'utf8')
-            }, app);
+            server.https = https.createServer(options, app);
         }
     }
     return server;
