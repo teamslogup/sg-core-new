d1ffb3809bdfd4020c0539c6bcc42fa56306ef56
Ryu YoungJin <ac12bd@gmail.com>
2017-01-26 21:49:28 +0900

    initialization 플래그 버그 수정

--- core/server/config/initialize-database.js
+++ core/server/config/initialize-database.js
@@ -5,7 +5,16 @@ var coreUtils = require('../utils');
 module.exports = function (callback) {
     coreUtils.initialization.initialize(function (status, data) {
         if (status == 204) {
-            return callback();
+
+            coreUtils.initialization.initMobileVersion(function (status, data) {
+
+                if (status == 204) {
+                    return callback();
+                } else {
+                    console.log(status, data);
+                }
+            });
+
         } else {
             console.log(status, data);
         }
--- core/server/utils/initialization.js
+++ core/server/utils/initialization.js
@@ -5,6 +5,8 @@ var errorHandler = require('sg-sequelize-error-handler');
 module.exports = {
     initialize: function (callback) {
         if (STD.flag.isUseRedis) {
+            callback(204);
+        } else {
             sequelize.transaction(function (t) {
                 console.log("initLoginHistories");
                 return sequelize.models.LoginHistory.destroy({
@@ -23,8 +25,57 @@ module.exports = {
                     callback(204);
                 }
             });
-        } else {
-            callback(204);
         }
+    },
+    initMobileVersion: function (callback) {
+
+        sequelize.transaction(function (t) {
+
+            return sequelize.models.MobileVersion.findOne({
+                where: {
+                    type: STD.mobile.osTypeAndroid
+                },
+                limit: 1,
+                transaction: t
+            }).then(function (data) {
+                if (data) {
+                    return true;
+                } else {
+                    return sequelize.models.MobileVersion.create({
+                        type: STD.mobile.osTypeAndroid,
+                        version: '0.0.1'
+                    }, {
+                        transaction: t
+                    });
+                }
+            }).then(function () {
+
+                return sequelize.models.MobileVersion.findOne({
+                    where: {
+                        type: STD.mobile.osTypeIos
+                    },
+                    limit: 1,
+                    transaction: t
+                });
+
+            }).then(function (data) {
+
+                if (data) {
+                    return true;
+                } else {
+                    return sequelize.models.MobileVersion.create({
+                        type: STD.mobile.osTypeIos,
+                        version: '0.0.1'
+                    }, {
+                        transaction: t
+                    });
+                }
+            });
+
+        }).catch(errorHandler.catchCallback(callback)).done(function (isSuccess) {
+            if (isSuccess) {
+                callback(204);
+            }
+        });
     }
 };
