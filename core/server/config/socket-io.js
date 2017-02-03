666847ebf1b375a92ece9b5700df6dcdb4afdd34
Ryu YoungJin <ac12bd@gmail.com>
2017-02-03 17:50:36 +0900

    socket port 변경

--- core/server/config/socket-io.js
+++ core/server/config/socket-io.js
@@ -10,8 +10,8 @@ module.exports = function (server, app) {
     var io;
 
     if (STD.flag.isUseChat) {
-        var http = require('http').Server(app);
-        io = require('socket.io')(http);
+        // var http = require('http').Server(app);
+        io = require('socket.io')(server.http);
 
         io.use(function (socket, next) {
             sessionMiddleware(socket.request, socket.request.res, next);
@@ -20,9 +20,9 @@ module.exports = function (server, app) {
 
         chat.init(io);
 
-        http.listen(3002, function () {
-            console.log('socket.io listening on 3002');
-        });
+        // http.listen(3002, function () {
+        //     console.log('socket.io listening on 3002');
+        // });
     }
 
     return server;
