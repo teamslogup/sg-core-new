2b006ac18315f1a2b8b4e0e8cb4e50885e44dd4c
Ryu YoungJin <ac12bd@gmail.com>
2017-02-03 16:47:08 +0900

    레디스 비밀번호  제거

--- core/server/chat/index.js
+++ core/server/chat/index.js
@@ -16,15 +16,15 @@ module.exports.init = function (io) {
         var redisUrl = url.parse(CONFIG.db.socketRedis);
 
         if (redisUrl.auth) {
-            redisAuth = redisUrl.auth.split(':');
+            // redisAuth = redisUrl.auth.split(':');
 
             var pub = redis.createClient(redisUrl.port, redisUrl.hostname, {
-                auth_pass: redisAuth[1]
+                // auth_pass: redisAuth[1]
             });
 
             var sub = redis.createClient(redisUrl.port, redisUrl.hostname, {
                 detect_buffers: true,
-                auth_pass: redisAuth[1]
+                // auth_pass: redisAuth[1]
             });
 
             io.adapter(redisAdapter({pubClient: pub, subClient: sub}));
