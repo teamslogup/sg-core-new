87e3ac3be5b93a62e453c34dc30051c06246156f
Ryu YoungJin <ac12bd@gmail.com>
2017-02-13 15:28:03 +0900

    localization: 국가코드 소문자로

--- core/server/metadata/localization.js
+++ core/server/metadata/localization.js
@@ -1,31 +1,31 @@
 var localization = {
-    "defaultCountry": "KR",
+    "defaultCountry": "kr",
     "defaultLanguage": "ko",
     "countries": {
-        "KR": {
+        "kr": {
             "name": "대한민국",
             "enName": "Korea",
             "code": "+82",
             "timeGap": 9
-        },
-        "US": {
-            "name": "USA",
-            "enName": "USA",
-            "code": "+1",
-            "timeGap": 0
         }
+        // "us": {
+        //     "name": "USA",
+        //     "enName": "USA",
+        //     "code": "+1",
+        //     "timeGap": 0
+        // }
     },
     "languages" : {
         "ko" : {
             "name" : "한국어",
             "enName" : "KOREAN",
-            "countries" : ["KR"]
-        },
-        "en" : {
-            "name" : "English",
-            "enName" : "English",
-            "countries" : ["US"]
+            "countries" : ["kr"]
         }
+        // "en" : {
+        //     "name" : "English",
+        //     "enName" : "English",
+        //     "countries" : ["us"]
+        // }
     }
 };
 
