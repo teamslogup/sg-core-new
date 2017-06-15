7bacc17d20e7cdc6df7e15074d0f9e0977075b73
Ryu YoungJin <ac12bd@gmail.com>
2017-06-15 10:50:35 +0900

    fix: no-session-chat

--- core/server/chat/no-session/middlewares.js
+++ core/server/chat/no-session/middlewares.js
@@ -112,8 +112,8 @@ var middles = {
             sequelize.models.NoSessionChatHistory.createNoSessionChatHistory(body, function (status, data) {
                 if (status == 200) {
 
-                    socket.emit(STD.chat.serverCheckMessage, body);
-                    socket.broadcast.to(roomId).emit(STD.chat.serverReceiveMessage, body);
+                    socket.emit(STD.chat.serverCheckMessage, data);
+                    socket.broadcast.to(roomId).emit(STD.chat.serverReceiveMessage, data);
 
                 } else {
                     return socket.emit(STD.chat.serverRequestFail, status, data);
