fad5157a4ac4db726749b01778da35c1607edd1b
KJY <dusrn@slogup.com>
2017-07-20 16:17:24 +0900

    1: unauthorized 이미지 url 받을 수 있도록 수정, 없을경우 기본 이미지 그대로 표시

--- core/client/components/image-url/core.common.image-url.js
+++ core/client/components/image-url/core.common.image-url.js
@@ -1,17 +1,21 @@
 export default function imageUrl (metaManager) {
     "ngInject";
 
-    return function (image, prefix) {
-        if (image && image.authorized) {
-            var imageName = image.name;
-            if (prefix) {
-                imageName = prefix + '_' + imageName;
+    return function (image, prefix, unauthorizedImageUrl) {
+        if (image) {
+            if (image.authorized || !unauthorizedImageUrl) {
+                var imageName = image.name;
+                if (prefix) {
+                    imageName = prefix + '_' + imageName;
+                }
+                var rootUrl = metaManager.std.cdn.rootUrl;
+                if (rootUrl && rootUrl[rootUrl.length - 1] == '/') {
+                    rootUrl = rootUrl.substr(0, rootUrl.length - 1);
+                }
+                return rootUrl + '/' + metaManager.std.file.folderImages + '/' + image.folder + '/' + image.dateFolder + '/' + imageName;
+            } else {
+                return unauthorizedImageUrl;
             }
-            var rootUrl = metaManager.std.cdn.rootUrl;
-            if (rootUrl && rootUrl[rootUrl.length - 1] == '/') {
-                rootUrl = rootUrl.substr(0, rootUrl.length - 1);
-            }
-            return rootUrl + '/' + metaManager.std.file.folderImages + '/' + image.folder + '/' + image.dateFolder + '/' + imageName;
         } else {
             return null;
         }
