4206e287eece4808411a92b5bcde83ca6a8511b6
Ryu YoungJin <ac12bd@slogup.com>
2017-08-22 17:54:40 +0900

    1: 회원 그룹채팅 기능

--- core/server/config/initialize-database.js
+++ core/server/config/initialize-database.js
@@ -3,36 +3,37 @@ var sequelize = require('../config/sequelize');
 var coreUtils = require('../utils');
 
 module.exports = function (callback) {
-    coreUtils.initialization.initialize(function (status, data) {
-        if (status == 204) {
-
-            coreUtils.initialization.initMobileVersion(function (status, data) {
-
-                if (status == 204) {
-
-                    coreUtils.initialization.initMassNotification(function (status, data) {
-                        if (status == 204) {
-
-                            coreUtils.initialization.initNoSessionChatRoomUser(function (status, data) {
-                                if (status == 204) {
-                                    return callback();
-                                } else {
-                                    console.log(status, data);
-                                }
-                            });
-
-                        } else {
-                            console.log(status, data);
-                        }
-                    });
-
-                } else {
-                    console.log(status, data);
-                }
-            });
-
-        } else {
-            console.log(status, data);
-        }
-    });
+    return callback();
+    // coreUtils.initialization.initialize(function (status, data) {
+    //     if (status == 204) {
+    //
+    //         coreUtils.initialization.initMobileVersion(function (status, data) {
+    //
+    //             if (status == 204) {
+    //
+    //                 coreUtils.initialization.initMassNotification(function (status, data) {
+    //                     if (status == 204) {
+    //
+    //                         coreUtils.initialization.initNoSessionChatRoomUser(function (status, data) {
+    //                             if (status == 204) {
+    //                                 return callback();
+    //                             } else {
+    //                                 console.log(status, data);
+    //                             }
+    //                         });
+    //
+    //                     } else {
+    //                         console.log(status, data);
+    //                     }
+    //                 });
+    //
+    //             } else {
+    //                 console.log(status, data);
+    //             }
+    //         });
+    //
+    //     } else {
+    //         console.log(status, data);
+    //     }
+    // });
 };
