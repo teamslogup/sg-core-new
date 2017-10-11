106fda97425872a4a16491ca384d4810b36c3fb7
KJY <dusrn@slogup.com>
2017-10-11 10:45:36 +0900

    1: core base module 수정 - 필수 모듈이 아닌 모듈 제거 >> 최적화

--- core/client/modules/common/base/core.base.module.js
+++ core/client/modules/common/base/core.base.module.js
@@ -3,8 +3,8 @@ import uiRouter from 'angular-ui-router';
 import translate from 'angular-translate';
 import cookies from 'angular-cookies';
 import ngResource from 'angular-resource';
-import uiBootstrap from 'angular-ui-bootstrap';
-import 'angular-file-upload';
+// import uiBootstrap from 'angular-ui-bootstrap';
+// import 'angular-file-upload';
 
 import metaManager from './meta/core.base.meta.manager';
 import errorHandler from './error-handler/core.error.handler';
@@ -23,18 +23,18 @@ import LoadingHandlerCtrl from './loading-handler/controllers/core.loading-handl
 import alertDialogService from './alert-dialog/services/core.alert-dialog.service';
 import AlertDialogCtrl from './alert-dialog/controllers/core.alert-dialog.controller';
 
-import 'angularjs-datepicker/dist/angular-datepicker.min.css';
-import 'angularjs-datepicker';
+// import 'angularjs-datepicker/dist/angular-datepicker.min.css';
+// import 'angularjs-datepicker';
 
 export default angular.module("core.base", [
     coreCommon,
     uiRouter,
     translate,
     cookies,
-    ngResource,
-    uiBootstrap,
-    'angularFileUpload',
-    '720kb.datepicker'
+    ngResource
+    // uiBootstrap,
+    // 'angularFileUpload',
+    // '720kb.datepicker'
 ])
     .provider('metaManager', metaManager)
     .service('errorHandler', errorHandler)
