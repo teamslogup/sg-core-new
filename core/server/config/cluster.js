a2d8203dfea98193c8de9470f73127c6a36ab7ef
hyeonseongjae <hyeonsj413@slogup.com>
2017-12-27 19:08:07 +0900

    1: cluster app 연동 추가 (/app/server/congift/cluster.js 추가 필요)

--- core/server/config/cluster.js
+++ core/server/config/cluster.js
@@ -1,6 +1,9 @@
 var cluster = require('cluster'),
     os = require('os');
 
+var fs = require('fs');
+var path = require('path');
+
 var config = require('../../../bridge/config/env');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -9,6 +12,16 @@ var restartWorkers = []; // que of restart workers
 
 var TIMEOUT = 120 * 1000; // 2 minutes
 var timeout;
+
+var clusterFunc = {};
+var appCluster = null;
+
+var exists = fs.existsSync(path.resolve(__dirname, '../../../app/server/config/cluster.js'));
+if(exists){
+    appCluster = require('../../../app/server/config/cluster');
+    clusterFunc.app = appCluster.initClusterFunc;
+}
+
 
 function initWorker (worker) {
     worker.on("message", function (msg) {
@@ -30,6 +43,10 @@ function initWorker (worker) {
             restartWorker(workers[0]);
         } else if (msg.cmd && msg.cmd == "close") {
             closeWorker(workers[0]);
+        }
+
+        if(appCluster != null) {
+            appCluster.initWorker(worker);
         }
     });
 
@@ -69,125 +86,133 @@ function processExit (serverCheck, code) {
     }
 }
 
-module.exports = {
-    startCluster: function (argServer) {
-        var app = argServer;
-        cluster.schedulingPolicy = cluster.SCHED_RR;
 
-        if(config.flag.isNotUseClusterRoundRobin){
-            cluster.schedulingPolicy = cluster.SCHED_NONE;
+clusterFunc.startCluster =  function (argServer) {
+    var app = argServer;
+    cluster.schedulingPolicy = cluster.SCHED_RR;
+
+    if(config.flag.isNotUseClusterRoundRobin){
+        cluster.schedulingPolicy = cluster.SCHED_NONE;
+    }
+
+    if (cluster.isMaster) {
+        workers = [];
+
+        var realCpuLength;
+        var customCpuLength = config.app.maxClusterLength;
+        var cpuLength = os.cpus().length;
+        if (customCpuLength == 0 || cpuLength < customCpuLength) {
+            realCpuLength = cpuLength;
+        } else {
+            realCpuLength = customCpuLength;
         }
 
-        if (cluster.isMaster) {
-            workers = [];
+        console.log("isMaster process running, cpu length:", realCpuLength);
 
-            var realCpuLength;
-            var customCpuLength = config.app.maxClusterLength;
-            var cpuLength = os.cpus().length;
-            if (customCpuLength == 0 || cpuLength < customCpuLength) {
-                realCpuLength = cpuLength;
+        for (var i = 0; i < realCpuLength; ++i){
+            workers.push(initWorker(cluster.fork()));
+        }
+
+        cluster.on("exit", function (worker, code, signal) {
+            // console.log("exit worker pid : " + worker.process.pid);
+            // console.log("exit worker code : " + code);
+            // console.log("exit worker signal : " + signal);
+
+            workers.splice(workers.indexOf(worker), 1);
+            if (code == 200) {
+                restartWorkers.push(worker);
+                console.log("restart workers length:", restartWorkers.length);
+                workers.push(initWorker(cluster.fork()));
+            } else if (code == 100) {
+                console.log("rest workers length:", workers.length);
+                if (workers.length > 0) closeWorker(workers[0]);
             } else {
-                realCpuLength = customCpuLength;
-            }
-
-            console.log("isMaster process running, cpu length:", realCpuLength);
-
-            for (var i = 0; i < realCpuLength; ++i){
+                console.log("unexpected exit");
                 workers.push(initWorker(cluster.fork()));
             }
-    
-            cluster.on("exit", function (worker, code, signal) {
-                // console.log("exit worker pid : " + worker.process.pid);
-                // console.log("exit worker code : " + code);
-                // console.log("exit worker signal : " + signal);
 
-                workers.splice(workers.indexOf(worker), 1);
-                if (code == 200) {
-                    restartWorkers.push(worker);
-                    console.log("restart workers length:", restartWorkers.length);
-                    workers.push(initWorker(cluster.fork()));
-                } else if (code == 100) {
-                    console.log("rest workers length:", workers.length);
-                    if (workers.length > 0) closeWorker(workers[0]);
-                } else {
-                    console.log("unexpected exit");
-                    workers.push(initWorker(cluster.fork()));
+        });
+
+    } else if (cluster.isWorker) {
+
+        var serverHttp = null;
+        var serverHttps = null;
+        var serverCheck = {};
+
+        if (app.http) {
+            serverCheck.http = true;
+            serverHttp = app.http.listen(config.app.port);
+        }
+        if (app.https) {
+            serverCheck.https = true;
+            serverHttps = app.https.listen(config.app.httpsPort);
+        }
+
+        process.send({cmd: "workerStart", pid: cluster.worker.process.pid});
+
+        process.on("message", function (msg) {
+            if (msg.cmd && msg.cmd == "close") {
+                process.send({cmd: "workerClose", pid: cluster.worker.process.pid});
+
+                // initiate graceful close
+                if (app.http) {
+                    gracefulCloseServer(serverHttp, 100, function (code) {
+                        delete serverCheck.http;
+                        processExit(serverCheck, code);
+                    });
                 }
-                
-            });
-
-        } else if (cluster.isWorker) {
-
-            var serverHttp = null;
-            var serverHttps = null;
-            var serverCheck = {};
-
-            if (app.http) {
-                serverCheck.http = true;
-                serverHttp = app.http.listen(config.app.port);
-            }
-            if (app.https) {
-                serverCheck.https = true;
-                serverHttps = app.https.listen(config.app.httpsPort);
-            }
-
-            process.send({cmd: "workerStart", pid: cluster.worker.process.pid});
-
-            process.on("message", function (msg) {
-                if (msg.cmd && msg.cmd == "close") {
-                    process.send({cmd: "workerClose", pid: cluster.worker.process.pid});
-
-                    // initiate graceful close
-                    if (app.http) {
-                        gracefulCloseServer(serverHttp, 100, function (code) {
-                            delete serverCheck.http;
-                            processExit(serverCheck, code);
-                        });
-                    }
-                    if (app.https) {
-                        gracefulCloseServer(serverHttps, 100, function (code) {
-                            delete serverCheck.https;
-                            processExit(serverCheck, code);
-                        });
-                    }
-                } else if (msg.cmd && msg.cmd == "restart") {
-                    process.send({cmd: "workerRestart", pid: cluster.worker.process.pid});
-
-                    // initiate graceful close
-                    if (app.http) {
-                        gracefulCloseServer(serverHttp, 200, function (code) {
-                            delete serverCheck.http;
-                            processExit(serverCheck, code);
-                        });
-                    }
-                    if (app.https) {
-                        gracefulCloseServer(serverHttps, 200, function (code) {
-                            delete serverCheck.https;
-                            processExit(serverCheck, code);
-                        });
-                    }
+                if (app.https) {
+                    gracefulCloseServer(serverHttps, 100, function (code) {
+                        delete serverCheck.https;
+                        processExit(serverCheck, code);
+                    });
                 }
-            });
-        } else {
-            console.log("start cluster error...")
-        }
-    },
-    closeCluster: function () {
-        if (cluster.isMaster) {
-            closeWorker(workers[0]);
-        } else if (cluster.isWorker) {
-            process.send({cmd: "close"});
-        } else {
-            console.log("close cluster error...");
-        }
-    },
-    restartCluster: function () {
-        if (cluster.isMaster) {
-            restartWorker(workers[0]);
-        } else if (cluster.isWorker) {
-            process.send({cmd: "restart"});
-        } else {
-            console.log("restart cluster error...");
-        }
+            } else if (msg.cmd && msg.cmd == "restart") {
+                process.send({cmd: "workerRestart", pid: cluster.worker.process.pid});
+
+                // initiate graceful close
+                if (app.http) {
+                    gracefulCloseServer(serverHttp, 200, function (code) {
+                        delete serverCheck.http;
+                        processExit(serverCheck, code);
+                    });
+                }
+                if (app.https) {
+                    gracefulCloseServer(serverHttps, 200, function (code) {
+                        delete serverCheck.https;
+                        processExit(serverCheck, code);
+                    });
+                }
+            }
+
+            if(appCluster != null) {
+                appCluster.initMessage(msg);
+            }
+        });
+    } else {
+        console.log("start cluster error...")
     }
 };
+
+clusterFunc.closeCluster = function () {
+    if (cluster.isMaster) {
+        closeWorker(workers[0]);
+    } else if (cluster.isWorker) {
+        process.send({cmd: "close"});
+    } else {
+        console.log("close cluster error...");
+    }
+};
+
+clusterFunc.restartCluster =  function () {
+    if (cluster.isMaster) {
+        restartWorker(workers[0]);
+    } else if (cluster.isWorker) {
+        process.send({cmd: "restart"});
+    } else {
+        console.log("restart cluster error...");
+    }
+};
+
+
+module.exports = clusterFunc;
